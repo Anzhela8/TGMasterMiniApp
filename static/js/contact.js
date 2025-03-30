49d4e8d7c95ef0db3a69ea9862fde44bb064dd47
Анжела <damn.angel.666@gmail.com>
2025-03-30 23:40:28 +0300

    Add files via upload

---
+++ static/js/contact.js
@@ -0,0 +1,5 @@
+#https://t.me/@am4I4I
+function contact() {
+    const tg = window.Telegram.WebApp;
+    tg.openTelegramLink('https://t.me/am4I4I'); // Замени YourUsername на свой Telegram-username
+}
